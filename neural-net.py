1dee8bfc8b065decb306bedc583f56848e1d96e2
EmmanuelManana <emanana@student.wethinkcode.co.za>
2020-08-03 19:46:22 +0200

    two different solutions to computing a layer of nuerons

--- neural-net.py
+++ neural-net.py
@@ -9,13 +9,20 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2.0, 3.0, 0.5]
 
+#simple hidden layer
 layer_output =[]
 for neuron_weight, neuron_bias in zip(weights, biases): # associate each bias with its weights ie. [([0.2, 0.8, -0.5, 1.0], 2.0), ...]
+    #dot product: input · weight = |weight| × |implut| × cos(θ) = suum of the products of weight and inputs
     neuron_output = 0
     for neuron_input, n_weight in zip(inputs, neuron_weight):
         neuron_output += neuron_input*n_weight
+    #add the bias
     neuron_output += neuron_bias
     layer_output.append(neuron_output)
 
 print(layer_output)
+
+#doing the exactly above using numpy
+l_output = np.dot(weights, inputs) + biases
+print(l_output)
    
