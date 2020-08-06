dcf336f4c5e9cc2ec7f0692c300c21c190980ff8
EmmanuelManana <emanana@student.wethinkcode.co.za>
2020-08-06 22:34:45 +0200

    ReLu, activation

--- neural-net-object.py
+++ neural-net-object.py
@@ -1,11 +1,26 @@
 import numpy as np 
 
-# X input feature set (training data set)
-X =[[1, 2, 3, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]]
-
 np.random.seed(0)
+# # X input feature set (training data set)
+# X =[[1, 2, 3, 2.5],
+#     [2.0, 5.0, -1.0, 2.0],
+#     [-1.5, 2.7, 3.3, -0.8]]
+
+#create dummp data
+def create_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number + 1))
+        r = np.linspace(0.0, 1, points) #radius
+        t = np.linspace(class_number*4, (class_number + 1)* 4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+
+X, y = create_data(100, 3)
+
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):# weight matrice dimension
         #load batch
@@ -14,11 +29,19 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
         
+class Activation_ReLu: #Rectified linear unit
+    def __init__(self):
+        pass
+    # ReLu ()
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+        
 
-hidden_layer1 = Layer_Dense(4, 3) # Dimesions 3 x 4 must multiply with 3 x anything , resulting in 4 x anything matrix
-hidden_layer2 = Layer_Dense(3, 3)
+hidden_layer1 = Layer_Dense(2, 5) # Dimesions 3 x 4 must multiply with 3 x anything , resulting in 3 x anything matrix
+Activation = Activation_ReLu()
 
 hidden_layer1.forward(X)
-hidden_layer2.forward(hidden_layer1.output)
-# should be a 3 by 4 matrice
-print(hidden_layer2.output)
+
+Activation.forward(hidden_layer1.output)
+print(hidden_layer1.output)
+print(Activation.output)
